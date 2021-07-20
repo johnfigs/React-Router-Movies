f89634543a5b2a114a3b7af9e1ada20f08b83aef
Johnathan Figueroa <johnathan.c.figueroa@gmail.com>
2021-07-19 22:31:31 -0700

    Added second route, completed changes to movie.js, and added home button navigation functionality

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,10 @@ import SavedList from './Movies/SavedList';
 
 //React Router imports {Router, Link, and Switch}
 import { Route, Link, Switch, Redirect } from 'react-router-dom';
+
 import MovieList from './Movies/MovieList';
+
+import Movie from './Movies/Movie';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -37,11 +40,12 @@ export default function App () {
       <SavedList list={[ /* This is stretch */]} />
 
       <Switch>
-        <Route path='/'>
+        <Route exact path='/'>
           <MovieList movies={movieList}/>
         </Route>
         <Route path='/movies/:id'>
           {/*take id parameter after movie(/movies/2). Should load the Movie component*/}
+          <Movie/>
         </Route>
       </Switch>
     </div>
